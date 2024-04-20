d373739ea2aa36bf7d62e8ecb86c349ceb52cc75
JoaquinRFM <131552885+JoaquinRFM@users.noreply.github.com>
2024-04-19 21:22:37 -0500

    Add files via upload

--- datos.sql
+++ datos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-04-2024 a las 01:43:10
+-- Tiempo de generación: 20-04-2024 a las 04:19:35
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -30,17 +30,25 @@ SET time_zone = "+00:00";
 CREATE TABLE `datos` (
   `id` int(20) NOT NULL,
   `nombre` varchar(50) NOT NULL,
-  `birth` varchar(20) NOT NULL,
+  `birth` varchar(50) NOT NULL,
   `ocupation` varchar(50) NOT NULL,
   `phone` int(20) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `country` varchar(50) NOT NULL,
+  `country` varchar(20) NOT NULL,
   `level` varchar(50) NOT NULL,
   `lenguages` varchar(50) NOT NULL,
   `aptitudes` varchar(50) NOT NULL,
   `skills` varchar(50) NOT NULL,
-  `fecha` varchar(200) NOT NULL
+  `perfil` varchar(500) NOT NULL,
+  `fecha` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `datos`
+--
+
+INSERT INTO `datos` (`id`, `nombre`, `birth`, `ocupation`, `phone`, `email`, `country`, `level`, `lenguages`, `aptitudes`, `skills`, `perfil`, `fecha`) VALUES
+(1, 'hola', '14/04/23', 'hola', 666666, 'hola@gmail.com', 'DE', 'on', 'C++', 'Proactividad', 'on', 'hyhtyntfyjbvty', '20/04/24');
 
 --
 -- Índices para tablas volcadas
@@ -60,7 +68,7 @@ ALTER TABLE `datos`
 -- AUTO_INCREMENT de la tabla `datos`
 --
 ALTER TABLE `datos`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
